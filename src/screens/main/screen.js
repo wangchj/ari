7a3b7911658b2185f3223b40d92a5e6835f35f6b
Jeff Wang <cjw39@hotmail.com>
2020-05-31 21:51:53 -0700

    Move styles to scss files.

--- src/screens/main/screen.js
+++ src/screens/main/screen.js
@@ -5,109 +5,40 @@ import Levels from 'business/levels';
 import Formats from 'business/formats';
 import RadioButtons from 'widgets/radio-buttons';
 import Button from 'widgets/button';
+import StyleSheet from 'scss/screens/main/screen';
 
-let MenuOption = props => {
-  let box = {margin: '0.5rem'};
-  let btn = {width: 300};
-
-  return (
-    <div style={box}>
-      <button className="btn btn-primary btn-lg" style={btn}
-              onClick={e => props.onOpSelect(props.option.id)}>
-        {props.option.label}
-      </button>
-    </div>
-  )
-}
-
-let MenuOptionRx = connect(null,
-  dispatch => ({onOpSelect: op => dispatch({type: 'SELECT_OP', op: op})})
-)(MenuOption)
-
-let container = {
-  display: 'flex',
-  flexDirection: 'column',
-  height: '100%',
-};
-
-let title = {
-  // width: '100%',
-  display: 'flex',
-  justifyContent: 'center',
-  fontSize: '6em',
-  paddingTop: '50px',
-  paddingBottom: '50px',
-  // border: '1px dashed black',
-};
-
-let options = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  backgroundColor: '#f8f9fa',
-  paddingTop: '50px',
-  paddingBottom: '50px',
-};
-
-let ops = {
-  marginBottom: '50px',
-};
 
 let activeStyle = {
   backgroundColor: '#2E86C1',
   color: '#FFF',
 };
 
-let table = {
-  borderRadius: '0.3em',
-};
-
-let t1 = {
-  paddingRight: '1.2em',
-  paddingBottom: '1em',
-};
-let t2 = {
-  paddingRight: '1.2em',
-  paddingTop: '1em',
-};
-let o1 = {
-  paddingBottom: '1em',
-};
-let o2 = {
-  paddingTop: '1em',
-};
-
-let finalize = {
-  paddingTop: '50px',
-  display: 'flex',
-  justifyContent: 'center',
-};
 let Screen = props => {
   let level  = props.options.level;
   let format = props.options.format;
 
   return (
-    <div style={container}>
-      <div style={title}>Ari Math</div>
-      <div style={options}>
-        <div style={ops}>
+    <div id="main-screen">
+      <div id="title">Ari Math</div>
+      <div id="options">
+        <div id="ops">
           <RadioButtons options={Operations} activeId={props.op}
             onClick={props.onOpClick}/>
         </div>
         <div>
-          <table cellSpacing="0" style={table}>
+          <table cellSpacing="0">
             <tbody>
               <tr>
-                <td style={t1}>Level</td>
-                <td style={o1}>
+                <td className="title">Level</td>
+                <td className="config">
                   <RadioButtons options={Levels} activeId={level}
                     activeStyle={activeStyle} size="s"
                     onClick={v => props.onOptionClick('level', v)} />
                 </td>
               </tr>
               <tr>
-                <td style={t2}>Format</td>
-                <td style={o2}>
+                <td className="title">Format</td>
+                <td className="config">
                   <RadioButtons options={Formats} activeId={format}
                   activeStyle={activeStyle} size="s"
                   onClick={v => props.onOptionClick('format', v)} />
@@ -117,7 +48,7 @@ let Screen = props => {
           </table>
         </div>
       </div>
-      <div style={finalize}>
+      <div id="finalize">
         <Button label="Start!" onClick={props.onStartClick}/>
       </div>
     </div>
--- src/widgets/button.js
+++ src/widgets/button.js
@@ -1,27 +1,8 @@
 import React from 'react';
-
-let fontSize = '1.2em';
-let bgColor = '#2C3E50';
-let textColor = '#fff';
-let borderRadius = '0.3em';
-let padH = '1.5em';
-let padV = '1em';
-let cursor = 'pointer';
-let textAlign = 'center';
-
-let style = {
-  fontSize: fontSize,
-  padding: `${padV} ${padH}`,
-  backgroundColor: bgColor,
-  color: textColor,
-  textAlign: textAlign,
-  borderRadius: borderRadius,
-  cursor: cursor,
-};
-
+import Style from 'scss/widgets/button';
 
 function Button(props) {
-  return <div style={style} onClick={props.onClick}>
+  return <div className="button" onClick={props.onClick}>
     {props.label}
   </div>
 }
