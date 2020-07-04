a616692d569df16d98d570fd4c7c1fdcf357fdc2
Jeff Wang <cjw39@hotmail.com>
2020-07-04 00:07:49 -0700

    Show the list of problems to run result screen.

--- src/screens/result/screen.js
+++ src/screens/result/screen.js
@@ -4,15 +4,35 @@ import Prob from 'business/problem';
 import Button from 'widgets/button';
 
 import ScreenStyle from 'scss/screens/result/screen';
+import RunInfoStyle from 'scss/screens/run/run-info';
+
+function Pill(props) {
+  let p = props.prob;
+  let r = Prob.isCorrect(p);
+  let a = Prob.eval(p);
+
+  return (
+    <div className="pill">
+      <div className="k">{p.vals[0]} {p.op} {p.vals[1]}</div>
+      <div className={'v ' + (r ? 'v-green' : 'v-red')}>
+        {r ? p.input : <span><s>{p.input}</s> ({a})</span>}
+      </div>
+    </div>
+  )
+}
 
 function Screen(props) {
   let probs = props.run.problems;
   let total = probs.length;
   let right = probs.reduce((a, v) => Prob.isCorrect(v) ? a + 1 : a, 0);
-  let p     = Math.round(right / total * 100);
 
   return (
     <div className="report-screen">
+      <div id="run-info">
+        <div>
+          <span className="exit" onClick={props.onDoneClick}>×</span>
+        </div>
+      </div>
       <div className="title">Run result</div>
       <div className="stats-area">
         <div className="stats">
@@ -30,8 +50,13 @@ function Screen(props) {
           </div>
         </div>
       </div>
-      <div className="buttons">
-        <Button label="Done!" variant="dark" onClick={props.onDoneClick}/>
+
+      <div className="problems">
+        <div className="pills">
+          {
+            probs.map((p, i) => <Pill key={i} prob={p}/>)
+          }
+        </div>
       </div>
     </div>
   )
